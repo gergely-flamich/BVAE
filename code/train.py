7b2efedb12bf0c84251a8793a0a44b0a5beff3c9
Gergely Flamich <gf332@cam.ac.uk>
2020-07-04 15:37:02 +0100

    Written a simple MNIST VAE

---
+++ code/train.py
@@ -0,0 +1,135 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+import datetime
+
+from sacred import Experiment
+
+from vae import VAE
+
+tfs = tf.summary
+
+ex = Experiment("bvae_experiment", ingredients=[])
+
+
+@ex.config
+def config():
+
+    data_dir = "/scratch/gf332/Misc/datasets/"
+    model_base_save_dir = "/scratch/gf332/Misc/bvae_experiments"
+
+    model = "vae"
+
+    batch_size = 64
+
+    # Gradient descent Optimizer
+    optimizer = "adam"
+    iterations = 100000
+    learning_rate = 1e-3
+
+    # Latent dimensions of the VAE
+    latent_dim = 50
+
+    # Logging
+    tensorboard_log_freq = 1000
+
+    model_save_dir = f"{model_base_save_dir}/{model}/latent_dim_{latent_dim}"
+
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_dir = f"{model_save_dir}/logs/{current_time}/train"
+
+
+@ex.automain
+def train(model_save_dir,
+          data_dir,
+          model,
+
+          batch_size,
+
+          optimizer,
+          iterations,
+          learning_rate,
+
+          latent_dim,
+          log_dir,
+          tensorboard_log_freq,
+
+          _log,
+          ):
+
+    # -------------------------------------------------------------------------
+    # Prepare the dataset
+    # -------------------------------------------------------------------------
+
+    data = tfds.load("mnist", data_dir=data_dir)
+
+    train_data = data["train"]
+    train_data = train_data.map(lambda x: tf.cast(x["image"], tf.float32) / 255.)
+    train_data = train_data.shuffle(5000)
+    train_data = train_data.repeat()
+    train_data = train_data.batch(batch_size)
+    train_data = train_data.prefetch(16)
+
+    # -------------------------------------------------------------------------
+    # Create model
+    # -------------------------------------------------------------------------
+
+    model = VAE(latent_dim=latent_dim)
+    model.build(input_shape=(batch_size, 28, 28, 1))
+
+    learning_rate = tf.Variable(learning_rate, dtype=tf.float32, name="learn_rate")
+
+    optimizer = {
+        "adam": tf.optimizers.Adam
+    }[optimizer](learning_rate=learning_rate)
+
+    ckpt = tf.train.Checkpoint(step=tf.Variable(1, dtype=tf.int64),
+                               learning_rate=learning_rate,
+                               model=model,
+                               optimizer=optimizer)
+
+    manager = tf.train.CheckpointManager(ckpt, model_save_dir, max_to_keep=3)
+
+    # -------------------------------------------------------------------------
+    # Create Summary Writer
+    # -------------------------------------------------------------------------
+    summary_writer = tfs.create_file_writer(log_dir)
+
+    # -------------------------------------------------------------------------
+    # Training Loop
+    # -------------------------------------------------------------------------
+
+    def train_step(model, batch):
+
+        with tf.GradientTape() as tape:
+
+            reconstructions = model(batch)
+
+            elbo = model.likelihood - model.kl_divergence
+
+            loss = -elbo
+
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        return reconstructions, model.likelihood, model.kl_divergence
+
+    for batch in train_data:
+
+        reconstructions, likelihood, kl_divergence = train_step(model, batch)
+
+        if int(ckpt.step) % tensorboard_log_freq == 0:
+            # Save model
+            save_path = manager.save()
+            _log.info(f"Step {int(ckpt.step)}: Saved model to {save_path}")
+
+            with summary_writer.as_default():
+                tfs.scalar(name="Likelihood", data=likelihood, step=ckpt.step)
+                tfs.scalar(name="KL", data=kl_divergence, step=ckpt.step)
+                tfs.scalar(name="ELBO", data=likelihood + kl_divergence, step=ckpt.step)
+
+                tfs.image(name="Original", data=batch, step=ckpt.step)
+                tfs.image(name="Reconstruction", data=reconstructions, step=ckpt.step)
+
+
+
---
+++ code/vae.py
@@ -0,0 +1,104 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+tfd = tfp.distributions
+tfl = tf.keras.layers
+
+
+class Encoder(tfl.Layer):
+
+    def __init__(self, latent_dim, name="encoder", *args, **kwargs):
+
+        super().__init__(name=name, *args, **kwargs)
+
+        self.latent_dim = latent_dim
+
+    def build(self, input_shape):
+
+        self.transforms = [
+            tfl.Reshape(target_shape=(28 * 28,)),
+            tfl.Dense(units=1024,
+                      activation=tf.nn.relu),
+            tfl.Dense(units=512,
+                      activation=tf.nn.relu),
+        ]
+
+        self.loc_head = tfl.Dense(units=self.latent_dim)
+        self.scale_head = tfl.Dense(units=self.latent_dim,
+                                    activation=tf.nn.softplus)
+
+        super().build(input_shape)
+
+    def call(self, inputs, **kwargs):
+
+        tensor = inputs
+
+        for layer in self.transforms:
+            tensor = layer(tensor)
+
+        loc = self.loc_head(tensor)
+        scale = self.scale_head(tensor)
+
+        return loc, scale
+
+
+class Decoder(tfl.Layer):
+
+    def __init__(self, name="decoder", *args, **kwargs):
+
+        super().__init__(name=name, *args, **kwargs)
+
+    def build(self, input_shape):
+
+        self.transforms = [
+            tfl.Dense(units=512,
+                      activation=tf.nn.relu),
+            tfl.Dense(units=1024,
+                      activation=tf.nn.relu),
+            tfl.Dense(units=28 * 28,
+                      activation=tf.nn.sigmoid),
+            tfl.Reshape(target_shape=(28, 28, 1)),
+        ]
+
+        super().build(input_shape)
+
+    def call(self, inputs, **kwargs):
+
+        tensor = inputs
+
+        for layer in self.transforms:
+            tensor = layer(tensor)
+
+        return tensor
+
+
+class VAE(tf.keras.Model):
+
+    def __init__(self, latent_dim, name="vae", *args, **kwargs):
+
+        super().__init__(name=name, *args, **kwargs)
+
+        self.latent_dim = latent_dim
+
+        self.encoder = Encoder(latent_dim=self.latent_dim)
+        self.decoder = Decoder()
+
+        self.prior = tfd.Normal(loc=0., scale=1.)
+
+    def call(self, tensor):
+
+        posterior_loc, posterior_scale = self.encoder(tensor)
+
+        self.posterior = tfd.Normal(loc=posterior_loc, scale=posterior_scale)
+
+        self.kl_divergence = tfd.kl_divergence(self.posterior, self.prior)
+        self.kl_divergence = tf.reduce_mean(tf.reduce_sum(self.kl_divergence, axis=1))
+
+        latent_code = self.posterior.sample()
+
+        reconstruction = self.decoder(latent_code)
+
+        self.likelihood_dist = tfd.Normal(loc=reconstruction, scale=1.)
+        self.likelihood = self.likelihood_dist.log_prob(tensor)
+        self.likelihood = tf.reduce_mean(tf.reduce_sum(self.likelihood, axis=[1, 2]))
+
+        return reconstruction
